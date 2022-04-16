fd91f4cfb4aaa4bf219a8047a081bb5f2856bd63
LebinHuang <103292721+LebinHuang@users.noreply.github.com>
2022-04-15 18:49:25 -0700

    test
    Exception in thread "main" java.lang.StringIndexOutOfBoundsException: begin 47, end -1, length 78
            at java.base/java.lang.String.checkBoundsBeginEnd(String.java:4601)
            at java.base/java.lang.String.substring(String.java:2704)
            at MarkdownParse.getLinks(MarkdownParse.java:20)
            at MarkdownParse.main(MarkdownParse.java:31)Exception in thread "main" java.lang.StringIndexOutOfBoundsException: begin 47, end -1, length 78
            at java.base/java.lang.String.checkBoundsBeginEnd(String.java:4601)
            at java.base/java.lang.String.substring(String.java:2704)
            at MarkdownParse.getLinks(MarkdownParse.java:20)
            at MarkdownParse.main(MarkdownParse.java:31)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,16 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        System.out.println(currentIndex);
         while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
+            int openBracket = markdown.indexOf("[",currentIndex );
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
         }
-
         return toReturn;
     }
 
