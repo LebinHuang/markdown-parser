328578721e690a665c3438277e5affbafdd7c59d
LebinHuang <103292721+LebinHuang@users.noreply.github.com>
2022-04-29 18:30:10 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -17,7 +17,7 @@ public class MarkdownParseTest {
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> test = new ArrayList<>();
         test.add("https://something.com");
-        test.add("some-thing.html");
+        test.add("some.html");
         assertEquals(test, links);
     }
 
